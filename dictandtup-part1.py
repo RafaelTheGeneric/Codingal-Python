15c1420fdf214d6606780cae1e566205b24b9e73
RafaelTheGeneric <ethan.rafael1212@gmail.com>
2025-02-15 21:06:34 +0600

    Add files via upload

---
+++ dictandtup-part1.py
@@ -0,0 +1,23 @@
+thedictionary = {}
+
+#dictonary with ingeter keys
+thedictionary = {2: 'apple', 3: 'ball'}
+
+
+
+thedictionary = {'name': 'John', 1:[2, 4, 3], 'age': 20}
+
+thedictionary = {'name': 'Jack', 'age': 26}
+
+print(thedictionary['name'])
+print(thedictionary.get('age'))
+
+#print(thedictionary[1])
+
+thedictionary.pop('age')
+print(thedictionary)
+
+
+thedictionary.clear()
+print(thedictionary)
+
