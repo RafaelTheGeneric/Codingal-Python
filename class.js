9613d0843c943752201c06f61edc090f94fbdc20
RafaelTheGeneric <ethan.rafael1212@gmail.com>
2025-04-24 22:16:13 +0600

    Add files via upload

--- class.js
+++ class.js
@@ -1,24 +1,80 @@
-var myForm = document.getElementById("myForm")
-var myInput = document.getElementById("myInput")
-var myItem = document.getElementById("myItem")
+window.onload = function() {
+
+var sec = 0;
+var milsec = 0;
+var minutes = 0;
+var appendsec = document.getElementById("sec");
+var appendmilsec = document.getElementById("milsec");
+var appendmin = document.getElementById("min");
+var buttonstart = document.getElementById("start");
+var buttonstop = document.getElementById("stop");
+var buttonreset = document.getElementById("reset");
+var Interval;
 
 
-myForm.addEventListener("submit",function(event) {
-    event.preventDefault();
-    createItem(myInput.value)
-})
+    function starttimer() {
+        if(milsec <= 9) {
+            appendmilsec.innerHTML = "0" + milsec;
+        }
+        if(mileseconds > 9) {
+            appendmilsec.innerHTML = sec;
+        }
+        if(mileseconds > 99) { 
+            sec++;
+            appendsec.innerhtml = sec < 10 ? "0" + sec : sec;
+            milsec = 0;
+            appendmilsec.innerhtml = "00"
+        }
+    }
 
 
-function createItem(inputItems) {
-    var itemHTML = `<li>${inputItems}
-        <button onclick="deleteElement(this)">Delete</button>
-    <li>`;
-    myInput.insertAdjacentHTML("beforeend", itemHTML)
-    myItem.value = "";
-    myInput = focus();
-}
+    start.onclick = function () {
+        clear(Interval);
+        Interval = setInterval(starttimer, 10);
+    }
 
 
-function deleteElement(elementToDelete) {
-    elementToDelete.parentElement.remove();
-}
+    stop.onclick = function () {
+        clear(Interval);
+    }
+    
+
+
+    reset.onclick = function () {
+        clear(Interval);
+        milesec = 0;
+        sec = 0;
+        min = 0;
+        appendmilsec.innerHTML = "00";
+        appendsec.innerHTML = "00";
+        appendmin.innerHTML = "00"
+    }
+
+
+    add10seconds.onclick = function () {
+        sec + 10
+    }
+
+    add1min.onclick = function () {
+        min + 1
+    }
+
+    add10min.onclick = function () {
+        min + 10
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
