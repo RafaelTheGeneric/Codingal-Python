ffee0fca84af19db055c7b5d5734946874cd59bb
RafaelTheGeneric <ethan.rafael1212@gmail.com>
2025-04-26 22:18:14 +0600

    Add files via upload

--- class.js
+++ class.js
@@ -1,80 +1,26 @@
-window.onload = function() {
+var dice;
+var dices = ['&#9856;', '&#9857;', '&#9858;', '&#9859;', '&#9860;', '&#9861;'];
+var stopped = true;
+var t;
 
-var sec = 0;
-var milsec = 0;
-var minutes = 0;
-var appendsec = document.getElementById("sec");
-var appendmilsec = document.getElementById("milsec");
-var appendmin = document.getElementById("min");
-var buttonstart = document.getElementById("start");
-var buttonstop = document.getElementById("stop");
-var buttonreset = document.getElementById("reset");
-var Interval;
+function change() {
+    var random = Math.floor(Math.random() * 6)
+    dice.innerHTML = dices[random]
+}
 
+function stopstart() {
 
-    function starttimer() {
-        if(milsec <= 9) {
-            appendmilsec.innerHTML = "0" + milsec;
-        }
-        if(mileseconds > 9) {
-            appendmilsec.innerHTML = sec;
-        }
-        if(mileseconds > 99) { 
-            sec++;
-            appendsec.innerhtml = sec < 10 ? "0" + sec : sec;
-            milsec = 0;
-            appendmilsec.innerhtml = "00"
-        }
+    if (stopped) {
+        stopped = false;
+        t = setInterval(change, 1);
+    } else {
+        clearInterval(t);
+        stopped = true;
     }
 
+}
 
-    start.onclick = function () {
-        clear(Interval);
-        Interval = setInterval(starttimer, 10);
-    }
-
-
-    stop.onclick = function () {
-        clear(Interval);
-    }
-    
-
-
-    reset.onclick = function () {
-        clear(Interval);
-        milesec = 0;
-        sec = 0;
-        min = 0;
-        appendmilsec.innerHTML = "00";
-        appendsec.innerHTML = "00";
-        appendmin.innerHTML = "00"
-    }
-
-
-    add10seconds.onclick = function () {
-        sec + 10
-    }
-
-    add1min.onclick = function () {
-        min + 1
-    }
-
-    add10min.onclick = function () {
-        min + 10
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-};
+window.onload = function () {
+    dice = document.getElementById("dice");
+    stopstart();
+}
